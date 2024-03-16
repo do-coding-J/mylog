24d8d637dddd7eda55bd0755af1ffb6a9c947188
JJ <ghks9705@gmail.com>
2024-03-16 19:34:25 +0900

    homework

--- bitcoin_automation/main.py
+++ bitcoin_automation/main.py
@@ -1,8 +1,16 @@
 import pyupbit
 
-access_key = 'stxke0a2rLTw8SaA14X1bPQoskS7eRsAPlrlHF8w'
-secret_key = 'wCUsmHh2ipIM2D3J2hM0huDAvidUrpqQDcBqZXra'
+# 회사
+# access_key = 'stxke0a2rLTw8SaA14X1bPQoskS7eRsAPlrlHF8w'
+# secret_key = 'wCUsmHh2ipIM2D3J2hM0huDAvidUrpqQDcBqZXra'
+# 집
+# access_key = 'stxke0a2rLTw8SaA14X1bPQoskS7eRsAPlrlHF8w'
+# secret_key = 'wCUsmHh2ipIM2D3J2hM0huDAvidUrpqQDcBqZXra'
+
 upbit = pyupbit.Upbit(access_key, secret_key)
 
 target_ticker = "KRW-BTC"
+target_interval = "minute1"
+
+print(pyupbit.get_ohlcv(target_ticker, target_interval))
 
