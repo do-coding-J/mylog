c5f3dbec9488ec57e214fe86d2fa31c6882af337
JJ <ghks9705@gmail.com>
2024-05-03 01:55:04 +0900

    여기까지

--- esp/wifi_test/main/main.c
+++ esp/wifi_test/main/main.c
@@ -5,28 +5,29 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
+#include "nvs_flash.h"
 
 #define TAG "WIFI_SCAN"
 
 static EventGroupHandle_t wifi_event_group;
 const int WIFI_CONNECTEC_BIT = BIT0;
 
-static void wifi_scan_done(void*arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+static void wifi_scan_done(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
 {
-    if(event_id == WIFI_EVENT_SCAN_DONE)
+    if (event_id == WIFI_EVENT_SCAN_DONE)
     {
         printf("wifi 스캔 완료\n");
-        int ap_num = 0;
+        uint16_t ap_num = 0;
         esp_wifi_scan_get_ap_num(&ap_num);
 
-        if(ap_num == 0)
+        if (ap_num == 0)
         {
             printf("주변에 스캔 된 wifi가 없습니다.\n");
             return;
         }
 
-        wifi_ap_record_t* ap_list = (wifi_ap_record_t*)malloc(sizeof(wifi_ap_record_t)*ap_num);
-        if(!ap_list)
+        wifi_ap_record_t *ap_list = (wifi_ap_record_t *)malloc(sizeof(wifi_ap_record_t) * ap_num);
+        if (!ap_list)
         {
             printf("메모리 할당 실패\n");
             return;
@@ -35,9 +36,9 @@ static void wifi_scan_done(void*arg, esp_event_base_t event_base, int32_t event_
         esp_wifi_scan_get_ap_records(&ap_num, ap_list);
 
         printf("스캔 된 wifi 목록\n");
-        for(int i=0; i<ap_num;i++)
+        for (int i = 0; i < ap_num; i++)
         {
-            printf("SSID : %s, RSSI : %d\n", (char*)ap_list[i].ssid, ap_list[i].rssi);
+            printf("SSID : %s, RSSI : %d\n", (char *)ap_list[i].ssid, ap_list[i].rssi);
         }
 
         free(ap_list);
@@ -47,19 +48,17 @@ static void wifi_scan_done(void*arg, esp_event_base_t event_base, int32_t event_
 void wifi_init_sta()
 {
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&*cfg));
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
     ESP_ERROR_CHECK(esp_wifi_set_storage((WIFI_STORAGE_RAM)));
 
-    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_scan_done, NULL));
-
+    // ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_scan_done, NULL));
+    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_scan_done, NULL);
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
 
     wifi_config_t wifi_config = {
         .sta = {
             .ssid = "curinginnos5G",
-            .password = "05200520"
-        }
-    };
+            .password = "05200520"}};
 
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
 
